7e66d50a301c01853619efcccddf684025da1ee0
Jan Maaß <git@antipost.de>
2023-11-22 12:09:35 +0100

    Add volume covers

--- pages/volumes/the-fellowship-of-the-ring.js
+++ pages/volumes/the-fellowship-of-the-ring.js
@@ -1,5 +1,6 @@
 import { volumes } from "@/lib/data";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function bookDetail() {
   const volume = volumes.find(({ slug }) => slug === "the-fellowship-of-the-ring");
@@ -13,6 +14,12 @@ export default function bookDetail() {
           <li key={slug}>{book.ordinal}: {book.title}</li>
         ))}
       </ul>
+      <Image
+        src="/images/the-fellowship-of-the-ring.png"
+        height={230}
+        width={140}
+        alt="Book cover: The Fellowship of the Ring"
+      />
     </>
   );
 }
--- pages/volumes/the-two-towers.js
+++ pages/volumes/the-two-towers.js
@@ -1,5 +1,6 @@
 import { volumes } from "@/lib/data";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function bookDetail() {
   const volume = volumes.find(({ slug }) => slug === "the-two-towers");
@@ -13,6 +14,12 @@ export default function bookDetail() {
           <li key={slug}>{book.ordinal}: {book.title}</li>
         ))}
       </ul>
+      <Image
+        src="/images/the-two-towers.png"
+        height={230}
+        width={140}
+        alt="Book cover: The Two Towers"
+      />
     </>
   );
 }
